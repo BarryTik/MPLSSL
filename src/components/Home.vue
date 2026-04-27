7f2fdb3169f4ec3a04b49604ed54e36cee75ec51
Barry <barrytik@gmail.com>
2026-04-26 21:36:32 -0500

    remove quickstart page replace with mplssl skeleton

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  import mplsslLogo from '../assets/mplssl.png'
+  import { ref } from 'vue'
+
+  const view = ref('start')
+
+  function reset() {
+    view.value = 'start'
+  }
+  function playerInfo() {
+    view.value = 'loading'
+  }
+  function sessionInfo() {
+    view.value = 'loading'
+  }
+</script>
+
+<template>
+  <section class="center">
+    <div class="hero">
+      <img :src="mplsslLogo" class="base" width="170" height="179" alt="" v-on:click="reset"/>
+    </div>
+    <div v-if="view == 'start'">
+      <p><code v-on:click="sessionInfo">Session Info</code> or <code v-on:click="playerInfo">Player Info</code></p>
+    </div>
+    <div v-if="view == 'loading'">
+      <p>loading...</p>
+    </div>
+  </section>
+</template>
